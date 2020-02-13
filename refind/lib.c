tianon/mirror-refind

commit 398d0e96626fa25b92a50876a0b0ce47239a8be7

Fix screen corruption bug on exiting EFI shell on some systems.

refind/lib.c
@@ -341,6 +341,8 @@ EFI_STATUS ReinitRefitLib(VOID)
           SelfRootDir = SelfVolume->RootDir;
     } // if
 
+    InitScreen();
+    SetupScreen();
     return FinishInitRefitLib();
 }
 

docs/refind/refind-install.html
@@ -347,6 +347,6 @@ available from Roderick W. Smith.
 This document was created by
 <A HREF="/cgi-bin/man/man2html">man2html</A>,
 using the manual pages.<BR>
-Time: 14:31:00 GMT, December 11, 2018
+Time: 14:22:18 GMT, January 24, 2019
 </BODY>
 </HTML>

include/version.h
@@ -10,5 +10,5 @@
  *
  */
 
-#define REFIND_VERSION L"0.11.4.2"
+#define REFIND_VERSION L"0.11.4.4"
 

commit de6d4385c22acf9b66cdc55a721f1fb97ec45536

Merge 398d0e96626fa25b92a50876a0b0ce47239a8be7 broke display of loaded banners; this fixes it

refind/lib.c
@@ -341,8 +341,6 @@ EFI_STATUS ReinitRefitLib(VOID)
           SelfRootDir = SelfVolume->RootDir;
     } // if
 
-    InitScreen();
-    SetupScreen();
     return FinishInitRefitLib();
 }
 

refind/main.c
@@ -448,6 +448,10 @@ EFI_STATUS StartEFIImage(IN REFIT_VOLUME *Volume,
 
     // re-open file handles
     ReinitRefitLib();
+    if (!IsDriver) {
+        InitScreen();
+        SetupScreen();
+    }
 
 bailout_unload:
     // unload the image, we don't care if it works or not...